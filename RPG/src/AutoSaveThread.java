77432e148ffaafb6c7dd7aad511db76cfcf511bd
Filip <xraku075@gmail.com>
2025-12-06 16:51:09 +0100

    Dodanie plików
    Dodanie:
    - Diagram stanów
    - Diagram UseCase
    - Opis klas i testy funkcjonalne
    - Dokumentacje użytkownika i dewelopera

--- RPG/src/AutoSaveThread.java
+++ RPG/src/AutoSaveThread.java
@@ -9,19 +9,15 @@ public class AutoSaveThread extends Thread {
 
     @Override
     public void run() {
-        System.out.println("AUTOSAVE start [DEBUG]");
 
         while(isRunning) {
             try {
-                System.out.println("AUTOSAVE sleep [DEBUG]");
                 Thread.sleep(10000);
-                System.out.println("AUTOSAVE start again [DEBUG]");
             } catch (InterruptedException e) {
                 return;
             }
 
             if(player.isAlive()) {
-                System.out.println("AUTOSAVE attempt [DEBUG]");
                 SaveManager.saveGame(player, player.getRound(), false);
                 player.setAutosaved(true);
             }
